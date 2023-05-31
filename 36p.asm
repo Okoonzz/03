fbce0580e85146deaa498158f10f563896f6da90
Thành Công <90641599+Okoonzz@users.noreply.github.com>
2023-05-31 23:22:14 +0700

    36p

--- 36p.asm
+++ 36p.asm
@@ -17,3 +17,26 @@ func:
         int 0x80
         leave
         ret
+       
+;;;;;;;something updated here;;;;;;;;;
+
+global _start
+_start:
+        call func
+        mov eax, 1
+        mov ebx, 0
+        int 0x80
+func:
+        push ebp
+        mov ebp, esp
+        sub esp, 4
+        mov [esp], byte 'o'
+        mov [esp+1], byte 'h'
+        mov eax, 4
+        mov ebx, 1
+        mov ecx, esp
+        mov edx, 2
+        int 0x80
+        mov esp, ebp
+        pop ebp
+        ret          
