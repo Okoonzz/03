c811a1101f7e5b810451f8d68651ed03857410b5
Thành Công <90641599+Okoonzz@users.noreply.github.com>
2023-06-01 17:21:19 +0700

    36p

--- 36p.asm
+++ 36p.asm
@@ -40,3 +40,9 @@ func:
         mov esp, ebp
         pop ebp
         ret          
+        
+        
+; first, pushing then mov esp, ebo is the same as instantiating the stack of that function to perform tasks on it
+; then we sub it also means we are moving esp to the top of the main stack
+; as for ‘leave’, it is the opposite to what we are doing before this in order to reallocate the stack of the called function
+; now, backward mov is actually updating esp from the substitution (top of the stack), popping the first element of the stack is to make it ready for next calls.
